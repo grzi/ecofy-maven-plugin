7decec0ce62fb2e5d23257aa79a8c3963f0769d1
grzi <jeremy.thulliez@gmail.com>
2018-04-24 23:06:15 +0200

    Added Mojo and compatibility with lambda

---
+++ src/main/java/org/jbandit/ecofy/mojo/MergeMojo.java
@@ -0,0 +1,15 @@
+package org.jbandit.ecofy.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "merge")
+public class MergeMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
